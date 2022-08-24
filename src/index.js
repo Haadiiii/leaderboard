126497321e46e8cb8bdcf481507f338e1670a9ee
Hamid Ali <hammali99@gmail.com>
2022-08-24 21:21:51 +0500

    Use of APIs JSON and Arrow functions created

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 import './index.css';
+import { list, button, form } from './modules/Elements.js';
 
-import { list } from './modules/Elements.js';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/F320Bghj02145Qwer76T/scores/';
 
-document.addEventListener('DOMContentLoaded', () => {
-  let str = '';
+button.addEventListener('click', () => {
+  fetch(url,
+    {
+      method: 'GET',
 
-  str
-    += `<ul id="items">
-              <li>Name: 200</li>
-              <li>Name: 100</li>
-              <li>Name: 350</li>
-              <li>Name: 150</li>
-              <li>Name: 70</li>
-            </ul>
-    `;
-  list.innerHTML = str;
+    })
+
+    .then((res) => res.json())
+    .then((data) => {
+      list.innerHTML = '';
+      return data.result.forEach((el) => {
+        const div = document.createElement('div');
+        const user = document.createElement('h1');
+        const scores = document.createElement('h1');
+        div.className = 'items';
+        user.innerHTML = el.user;
+        scores.innerHTML = el.score;
+        div.append(user);
+        div.append(scores);
+        list.append(div);
+      });
+    });
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const payLoad = formData;
+  console.log(payLoad);
+  console.log('this shown content', ...payLoad);
+
+  const extractedPayLoad = [...payLoad];
+  const userValue = extractedPayLoad[0][1];
+  const scoreValue = extractedPayLoad[1][1];
+
+  const payLoadObject = {
+    user: userValue,
+    score: scoreValue,
+  };
+  console.log(payLoadObject);
+
+  fetch(url,
+    {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(payLoadObject),
+    })
+
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data', data);
+    });
 });
--- src/modules/Elements.js
+++ src/modules/Elements.js
@@ -1,1 +1,3 @@
 export const list = document.getElementById('list');
+export const button = document.getElementById('button');
+export const form = document.getElementById('form');
